3e65871515086ec16028ff14b295205884570c9d
Stone Lasley <slasley@blendtec.com>
2014-04-27 23:30:41 -0600

    Blacklist add/edit tests

--- Test/Case/Controller/SeoBlacklistsControllerTest.php
+++ Test/Case/Controller/SeoBlacklistsControllerTest.php
@@ -17,6 +17,21 @@ class SeoBlacklistsControllerTest extends ControllerTestCase {
 	);
 
 /**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->testData = array (
+			'id' => '535dda88-f5bc-4942-b9b2-1174173cdfff',
+			'ip_range_start' => '10.100.0.1',
+			'ip_range_end' => '10.100.0.2',
+			'note' => 50,
+			'is_active' => true
+		);
+	}
+/**
  * testBanned method
  *
  * @return void
@@ -46,14 +61,117 @@ class SeoBlacklistsControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testAdminAdd() {
+		$SeoBlacklists = $this->generate(
+			'Seo.SeoBlacklists', array (
+				'models' => array ('Seo.SeoBlacklist' => array ('save', 'create')),
+				'components' => array ('Session', 'Security')
+			)
+		);
+		$SeoBlacklists->Session->expects($this->once())
+			->method('setFlash')
+			->with(__('The seo blacklist has been saved'), 'default');
+		$SeoBlacklists->SeoBlacklist->expects($this->once())
+			->method('create');
+		$SeoBlacklists->SeoBlacklist->expects($this->once())
+			->method('save')
+			->will($this->returnValue(true)); //success
+		unset($this->testData['id']);
+		$this->testAction(
+			'admin/seo/seo_a_b_tests/add',
+			array (
+				'data' => array ('SeoBlacklist' => $this->testData),
+				'method' => 'post'
+			)
+		);
+		$this->assertTrue(isset($this->vars));
+		$this->assertEquals($this->vars['model'], 'SeoBlacklist');
 	}
 
 /**
- * testAdminEdit method
+ * testAdminAddFail method
  *
  * @return void
  */
-	public function testAdminEdit() {
+	public function testAdminAddFail() {
+		$SeoBlacklists = $this->generate(
+			'Seo.SeoBlacklists', array (
+				'models' => array ('Seo.SeoBlacklist' => array ('save', 'create')),
+				'components' => array ('Session', 'Security')
+			)
+		);
+		$SeoBlacklists->Session->expects($this->once())
+			->method('setFlash')
+			->with(__('The seo blacklist could not be saved. Please, try again.'), 'default');
+		$SeoBlacklists->SeoBlacklist->expects($this->once())
+			->method('create');
+		$SeoBlacklists->SeoBlacklist->expects($this->once())
+			->method('save')
+			->will($this->returnValue(false)); //success
+		unset($this->testData['id']);
+		$result = $this->testAction(
+			'admin/seo/seo_a_b_tests/add',
+			array (
+				'data' => array ('SeoBlacklist' => $this->testData),
+				'method' => 'post',
+				'return' => 'vars'
+			)
+		);
+		$this->assertTrue(isset($this->vars));
+		$this->assertEquals($this->vars['model'], 'SeoBlacklist');
+	}
+
+/**
+ * testAdminEditWithData method
+ *
+ * @return void
+ */
+	public function testAdminEditWithData() {
+		$SeoBlacklists = $this->generate(
+			'Seo.SeoBlacklists', array (
+				'models' => array ('Seo.SeoBlacklist' => array ('save', 'create')),
+				'components' => array ('Session', 'Security')
+			)
+		);
+		$SeoBlacklists->Session->expects($this->once())
+			->method('setFlash')
+			->with(__('The seo blacklist has been saved'), 'default');
+		$SeoBlacklists->SeoBlacklist->expects($this->once())
+			->method('save')
+			->will($this->returnValue(true)); //success
+		$this->testAction(
+			'admin/seo/seo_a_b_tests/edit',
+			array (
+				'data' => array ('SeoBlacklist' => $this->testData),
+				'method' => 'post'
+			)
+		);
+		$this->assertTrue(isset($this->vars));
+		$this->assertEquals($this->vars['model'], 'SeoBlacklist');
+	}
+
+/**
+ * testAdminEditWithNoId method
+ *
+ * @return void
+ */
+	public function testAdminEditWithNoId() {
+		$SeoBlacklists = $this->generate(
+			'Seo.SeoBlacklists', array (
+				'models' => array ('Seo.SeoBlacklist' => array ('save', 'create')),
+				'components' => array ('Session', 'Security')
+			)
+		);
+		$SeoBlacklists->Session->expects($this->once())
+			->method('setFlash')
+			->with(__('Invalid seo blacklist'), 'default');
+		$this->testAction(
+			'admin/seo/seo_a_b_tests/edit',
+			array (
+				'method' => 'get'
+			)
+		);
+		$this->assertTrue(isset($this->vars));
+		$this->assertEquals($this->vars['model'], 'SeoBlacklist');
 	}
 
 /**
