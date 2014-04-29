38836df67bbb85b1202ee88a7cd3089d1c8117d8
Stone Lasley <slasley@blendtec.com>
2014-04-28 20:51:58 -0600

    Updated Canonical Controller add

--- Controller/SeoCanonicalsController.php
+++ Controller/SeoCanonicalsController.php
@@ -18,11 +18,11 @@ class SeoCanonicalsController extends SeoAppController {
 	}
 
 	public function admin_add() {
-		if (!empty($this->request->data)) {
-			$this->SeoCanonical->clear();
+		if ($this->request->is('post')) {
+			$this->SeoCanonical->create();
 			if ($this->SeoCanonical->save($this->request->data)) {
 				$this->Session->setFlash(__('The seo canonical has been saved'));
-				$this->redirect(array('action' => 'index'));
+				return $this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The seo canonical could not be saved. Please, try again.'));
 			}
