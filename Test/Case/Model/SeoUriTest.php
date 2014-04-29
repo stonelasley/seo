9d5ee85a9558bfef8d401a9a1872cfb64eabf3e7
Stone Lasley <slasley@blendtec.com>
2014-04-28 23:13:11 -0600

    hulk smashgit add Test/Case/Model/SeoUriTest.php !

--- Test/Case/Model/SeoUriTest.php
+++ Test/Case/Model/SeoUriTest.php
@@ -51,7 +51,6 @@ class SeoUriTest extends CakeTestCase {
 		$this->assertTrue($this->SeoUri->field('is_approved'));
 	}
 
-
 	//	public function testSendNotification() {
 	//		$this->SeoUri->id = '5313a904-ca7c-4cee-9aed-0c05ccb469e7';
 	//		//$this->SeoUri->Email->expectOnce('send');
