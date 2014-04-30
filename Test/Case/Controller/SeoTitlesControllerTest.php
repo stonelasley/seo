f6d542c8fc28f6a8621648edd99764efe87bb134
Stone Lasley <slasley@blendtec.com>
2014-04-29 19:52:54 -0600

    TitlesController view/delete tests

--- Test/Case/Controller/SeoTitlesControllerTest.php
+++ Test/Case/Controller/SeoTitlesControllerTest.php
@@ -7,13 +7,20 @@ App::uses('SeoTitlesController', 'Seo.Controller');
  */
 class SeoTitlesControllerTest extends ControllerTestCase {
 
+
+/**
+ * Mock Controller
+ *
+ */
+	public $mockController;
 /**
  * Fixtures
  *
  * @var array
  */
 	public $fixtures = array(
-		'plugin.seo.seo_title'
+		'plugin.seo.seo_title',
+		'plugin.seo.seo_uri'
 	);
 
 /**
@@ -23,6 +30,12 @@ class SeoTitlesControllerTest extends ControllerTestCase {
  */
 	public function setUp() {
 		parent::setUp();
+		$this->mockController = $this->generate(
+			'Seo.SeoTitles', array (
+				'models' => array ('Seo.SeoTitle' => array ('save', 'create')),
+				'components' => array ('Session', 'Security')
+			)
+		);
 		$this->testData = array (
 			'id' => '535dab35-c600-4721-8a78-1175173cdfff',
 			'seo_uri_id' => '535da751-0100-409c-9adb-1173173cdfff',
@@ -46,6 +59,17 @@ class SeoTitlesControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testAdminView() {
+		$id = $this->testData['id'];
+		$this->mockController->SeoTitle->expects($this->once())
+			->method('exists')
+			->will($this->returnValue(true));
+
+		$this->testAction(
+			"admin/seo/seo_titles/view/$id",
+			array('return' => 'vars')
+		);
+		$this->assertTrue(isset($this->vars['seoTitle']));
+		$this->assertEquals($this->vars['model'], 'SeoTitle');
 	}
 
 /**
@@ -54,18 +78,12 @@ class SeoTitlesControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testAdminAdd() {
-		$SeoTitles = $this->generate(
-			'Seo.SeoTitles', array (
-				'models' => array ('Seo.SeoTitle' => array ('save', 'create')),
-				'components' => array ('Session', 'Security')
-			)
-		);
-		$SeoTitles->Session->expects($this->once())
+		$this->mockController->Session->expects($this->once())
 			->method('setFlash')
 			->with(__('The seo title has been saved'), 'default');
-		$SeoTitles->SeoTitle->expects($this->once())
+		$this->mockController->SeoTitle->expects($this->once())
 			->method('create');
-		$SeoTitles->SeoTitle->expects($this->once())
+		$this->mockController->SeoTitle->expects($this->once())
 			->method('save')
 			->will($this->returnValue(true));
 		unset($this->testData['id']);
@@ -86,18 +104,12 @@ class SeoTitlesControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testAdminAddFail() {
-		$SeoTitles = $this->generate(
-			'Seo.SeoTitles', array (
-				'models' => array ('Seo.SeoTitle' => array ('save', 'create')),
-				'components' => array ('Session', 'Security')
-			)
-		);
-		$SeoTitles->Session->expects($this->once())
+		$this->mockController->Session->expects($this->once())
 			->method('setFlash')
 			->with(__('The seo title could not be saved. Please, try again.'), 'default');
-		$SeoTitles->SeoTitle->expects($this->once())
+		$this->mockController->SeoTitle->expects($this->once())
 			->method('create');
-		$SeoTitles->SeoTitle->expects($this->once())
+		$this->mockController->SeoTitle->expects($this->once())
 			->method('save')
 			->will($this->returnValue(false));
 		unset($this->testData['id']);
@@ -119,16 +131,10 @@ class SeoTitlesControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testAdminEditWithData() {
-		$SeoTitles = $this->generate(
-			'Seo.SeoTitles', array (
-				'models' => array ('Seo.SeoTitle' => array ('save', 'create')),
-				'components' => array ('Session', 'Security')
-			)
-		);
-		$SeoTitles->Session->expects($this->once())
+		$this->mockController->Session->expects($this->once())
 			->method('setFlash')
 			->with(__('The seo title has been saved'), 'default');
-		$SeoTitles->SeoTitle->expects($this->once())
+		$this->mockController->SeoTitle->expects($this->once())
 			->method('save')
 			->will($this->returnValue(true));
 		$this->testAction(
@@ -148,13 +154,7 @@ class SeoTitlesControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testAdminEditWithNoId() {
-		$SeoTitles = $this->generate(
-			'Seo.SeoTitles', array (
-				'models' => array ('Seo.SeoTitle' => array ('save', 'create')),
-				'components' => array ('Session', 'Security')
-			)
-		);
-		$SeoTitles->Session->expects($this->once())
+		$this->mockController->Session->expects($this->once())
 			->method('setFlash')
 			->with(__('Invalid seo title'), 'default');
 		$this->testAction(
@@ -173,19 +173,13 @@ class SeoTitlesControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testAdminEditSaveFail() {
-		$SeoTitles = $this->generate(
-			'Seo.SeoTitles', array (
-				'models' => array ('Seo.SeoTitle' => array ('save', 'create')),
-				'components' => array ('Session', 'Security')
-			)
-		);
-		$SeoTitles->Session->expects($this->once())
+		$this->mockController->Session->expects($this->once())
 			->method('setFlash')
 			->with(__('The seo title could not be saved. Please, try again.'), 'default');
-		$SeoTitles->SeoTitle->expects($this->once())
+		$this->mockController->SeoTitle->expects($this->once())
 			->method('save')
 			->will($this->returnValue(false));
-		$result = $this->testAction(
+		$this->testAction(
 			'admin/seo/seo_titles/edit',
 			array (
 				'data' => array ('SeoTitle' => $this->testData),
@@ -203,6 +197,63 @@ class SeoTitlesControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testAdminDelete() {
+		$id = $this->testData['id'];
+		$this->mockController->SeoTitle->expects($this->once())
+			->method('exists')
+			->will($this->returnValue(true));
+		$this->mockController->SeoTitle->expects($this->once())
+			->method('delete')
+			->will($this->returnValue(true));
+		$this->mockController->Session->expects($this->once())
+			->method('setFlash')
+			->with(__('The seo title has been deleted.'), 'default');
+		$this->testAction(
+			"admin/seo/seo_titles/delete/$id",
+			array('return' => 'vars')
+		);
+
+		$this->assertStringEndsWith('/admin/seo/seo_titles', $this->headers['Location']);
+	}
+
+/**
+ * testAdminDeleteFails method
+ *
+ * @return void
+ */
+	public function testAdminDeleteFails() {
+		$id = $this->testData['id'];
+		$this->mockController->SeoTitle->expects($this->once())
+			->method('exists')
+			->will($this->returnValue(true));
+		$this->mockController->SeoTitle->expects($this->once())
+			->method('delete')
+			->will($this->returnValue(false));
+		$this->mockController->Session->expects($this->once())
+			->method('setFlash')
+			->with(__('The seo title could not be deleted. Please, try again.'), 'default');
+		$this->testAction(
+			"admin/seo/seo_titles/delete/$id",
+			array('return' => 'vars')
+		);
+		$this->assertStringEndsWith('/admin/seo/seo_titles', $this->headers['Location']);
+	}
+
+/**
+ * testAdminDeleteInvalidId method
+ *
+ * @return void
+ */
+	public function testAdminDeleteInvalidId() {
+		$id = 'Invalid';
+		$this->mockController->SeoTitle->expects($this->once())
+			->method('exists')
+			->will($this->returnValue(false));
+		$this->setExpectedException('NotFoundException');
+		$this->testAction(
+			"admin/seo/seo_titles/delete/$id",
+			array('return' => 'vars')
+		);
+		$this->assertStringEndsWith('/admin/seo/seo_titles', $this->headers['Location']);
 	}
 
 }
